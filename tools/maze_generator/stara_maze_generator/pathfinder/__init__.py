e015da76727414f645865b286641b72c9b91383d
Valerius Mattfeld <git@valerius.me>
2025-06-04 21:30:15 +0200

    Add submodules for A* algorithm implementations and tools

--- tools/maze_generator/stara_maze_generator/pathfinder/__init__.py
+++
@@ -1,4 +0,0 @@
-from stara_maze_generator.pathfinder.types import Pathfinder
-from stara_maze_generator.pathfinder.bfs import BFS
-
-__all__ = ["Pathfinder", "BFS"]
--- tools/maze_generator/stara_maze_generator/pathfinder/types.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum, auto
-
-
-class Pathfinder(Enum):
-    """Enum for different pathfinding algorithms."""
-
-    BFS = auto()
